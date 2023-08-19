ba78d3a420558ce78680ee2dfdc1822271fa0b48
md.umar05@gmail.com <md.umar05@gmail.com>
2023-08-19 18:20:18 +0530

    fotter done

--- components/screens/footer.tsx
+++ components/screens/footer.tsx
@@ -2,6 +2,14 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <footer className='mb-8 px-4 text-center text-gray-500'>
+        <small className='mb-2 block text-xs'>
+            &copy; 2023 Umar Ibn Shafee. All rights reserved
+        </small>
+        <p className='text-xs'>
+            <span className='font-semibold'>About this website:</span> built with react & Next.js (App Router & Server Actions), 
+            TypeScript, Tailwind CSS, Framer Motion, React Email & Resend, Vercel hosting. 
+        </p>
+    </footer>
   )
 }
