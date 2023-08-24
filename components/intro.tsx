43d9967d3ebc96dcc6cd25774696a89a0d563f3c
md.umar05@gmail.com <md.umar05@gmail.com>
2023-08-25 00:55:17 +0530

    intro text fix

--- components/intro.tsx
+++ components/intro.tsx
@@ -29,9 +29,9 @@ export default function Intro() {
                 className='bg-[#f63b6d] animate-go-rounds animation-delay-2000 transition-all mix-blend-multiply 
                 .animation-delay-4000 absolute -z-10 -bottom-[25rem] right-[5rem] h-[25rem] blur-[10rem] 
                 w-[25rem] rounded-full dark:bg-[#f63b6d] dark:mix-blend-normal'></div>
-            <div className='flex items-center justify-center'>
-                <div className='relative'>
-                    <motion.div
+            {/* <div className='flex items-center justify-center'> */}
+            {/* <div className='relative'> */}
+            {/* <motion.div
                         initial={{ opacity: 0, scale: 0 }}
                         animate={{ opacity: 1, scale: 1 }}
                         transition={{
@@ -47,8 +47,8 @@ export default function Intro() {
                             quality={95}
                             priority={true}
                             className='h-40 w-40 rounded-3xl object-cover border-[0.35rem] border-black shadow-xl' />
-                    </motion.div>
-                    {/* <motion.span className='text-6xl absolute bottom-0 -right-5'
+                    </motion.div> */}
+            {/* <motion.span className='text-6xl absolute bottom-0 -right-5'
                         initial={{ opacity: 0, scale: 0 }}
                         animate={{ opacity: 1, scale: 1 }}
                         transition={{
@@ -60,71 +60,73 @@ export default function Intro() {
                     >
                         👋
                     </motion.span> */}
-                </div>
-            </div>
+            {/* </div> */}
+            {/* </div> */}
 
             {/* max-w-[50rem] */}
 
-            <motion.h1
-                className='mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl'
-                initial={{ opacity: 0, y: 100 }}
-                animate={{ opacity: 1, y: 0 }}
-            >
-                <span className="font-bold">Hello, I&apos;m Umar.</span> I&apos;m a{" "}
-                <span className="font-bold">full-stack developer</span> with{" "}
-                <span className="font-bold">6 years</span> of experience. I enjoy
-                building <span className="italic">sites & apps</span>. My focus is{" "}
-                <span className="underline">React (Next.js)</span>.
-            </motion.h1>
+            <div className='flex flex-col items-start justify-center h-[90vh] sm:px-44 px-12'>
+                <motion.h1
+                    className='mb-10 px-4 text-left text-2xl font-normal !leading-[1.5] sm:text-4xl'
+                    initial={{ opacity: 0, y: 100 }}
+                    animate={{ opacity: 1, y: 0 }}
+                >
+                    Hello, I&apos;m <span className="font-bold">Umar Ibn Shafee</span> a{" "}
+                    <span className="font-bold font-mono">Full-stack developer</span><br/> with{" "}
+                    <span className="font-bold">6 years</span> of experience. I enjoy
+                    building <span className="italic">websites, SPA's & PWA's</span>. My focus is{" "}
+                    <span className="underline">Frontend (React/Next.js, Angular, Vue)</span>.
+                </motion.h1>
 
-            <motion.div
-                className='flex gap-3 px-4 flex-col sm:flex-row items-center justify-center text-lg font-medium'
-                initial={{ y: 100, opacity: 0 }}
-                animate={{ y: 0, opacity: 1 }}
-                transition={{
-                    delay: 0.2
-                }}
-            >
-                <Link
-                    href={"#contact"}
-                    className='group bg-gray-900 text-white px-7 py-3 flex justify-center items-center gap-2 
+                <motion.div
+                    className='flex gap-3 px-4 flex-col sm:flex-row items-center justify-center text-lg font-medium'
+                    initial={{ y: 100, opacity: 0 }}
+                    animate={{ y: 0, opacity: 1 }}
+                    transition={{
+                        delay: 0.2
+                    }}
+                >
+                    <Link
+                        href={"#contact"}
+                        className='group bg-gray-900 text-white px-7 py-3 flex justify-center items-center gap-2 
                     rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 
                     transition-all'
-                    onClick={() => setActiveSection('Contact')}
-                >
-                    Contact me here <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
-                </Link>
+                        onClick={() => setActiveSection('Contact')}
+                    >
+                        Contact me here <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+                    </Link>
 
-                <a
-                    className='group bg-white px-7 py-3 flex justify-center items-center gap-2 rounded-full 
+                    <a
+                        className='group bg-white px-7 py-3 flex justify-center items-center gap-2 rounded-full 
                     outline-none focus:scale-110 hover:scale-110 active:scale-105 transition-all border 
                     border-black/10 dark:bg-white/10'
-                    href='/Umar-CV.pdf'
-                    download={true}
-                >
-                    Download CV <HiDownload className="opacity-70 group-hover:translate-y-1 transition" />
-                </a>
+                        href='/Umar-CV.pdf'
+                        download={true}
+                    >
+                        Download CV <HiDownload className="opacity-70 group-hover:translate-y-1 transition" />
+                    </a>
 
-                <a
-                    className='bg-white p-4 text-gray-600 hover:text-gray-950 flex justify-center items-center 
+                    <a
+                        className='bg-white p-4 text-gray-600 hover:text-gray-950 flex justify-center items-center 
                     gap-2 rounded-full outline-none focus:scale-25 hover:scale-125 active:scale-105 transition-all 
                     border border-black/10 dark:bg-white/10 dark:text-white/70'
-                    href='https://www.linkedin.com/in/umar-ibn-shafee/'
-                    target='_blank'
-                >
-                    <BsLinkedin />
-                </a>
+                        href='https://www.linkedin.com/in/umar-ibn-shafee/'
+                        target='_blank'
+                    >
+                        <BsLinkedin />
+                    </a>
 
-                <a
-                    className='bg-white p-4 text-gray-600 hover:text-gray-950 flex justify-center items-center 
+                    <a
+                        className='bg-white p-4 text-gray-600 hover:text-gray-950 flex justify-center items-center 
                     gap-2 rounded-full outline-none focus:scale-125 hover:scale-125 active:scale-105 transition-all
                     border border-black/10 dark:bg-white/10 dark:text-white/70'
-                    href='https://github.com/umar-ibn-shafee'
-                    target='_blank'
-                >
-                    <BsGithub />
-                </a>
-            </motion.div>
+                        href='https://github.com/umar-ibn-shafee'
+                        target='_blank'
+                    >
+                        <BsGithub />
+                    </a>
+                </motion.div>
+            </div>
         </section>
     )
 }
