937e4a118aab5d8a93d177a7c76255635e209a8f
md.umar05@gmail.com <md.umar05@gmail.com>
2023-08-30 01:16:29 +0530

    experience section fixed

--- components/experience.tsx
+++ components/experience.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useSectioninview } from '@/lib/hooks'
-import React from 'react'
+import React, { useState } from 'react'
 import SectionHeading from './generic/section-heading'
 
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
@@ -12,7 +12,23 @@ import { useThemeContext } from '@/context/theme-context';
 export default function Experience() {
 
   const { ref } = useSectioninview('Experience')
-  const {theme} = useThemeContext()
+  const { theme } = useThemeContext()
+  const [showMore, setShowMore] = useState<number[]>([])
+
+  const shortDescription = (text: string) => {
+    const string150 = text.slice(0, 150)
+    return string150.slice(0, string150.lastIndexOf(' '))
+  }
+
+  const handleShowMore = (index: number) => {
+    return () => {
+      if (!showMore.includes(index)) {
+        setShowMore([...showMore, index])
+      } else {
+        setShowMore(prev => prev.filter(item => item !== index))
+      }
+    }
+  }
 
   return (
     <section ref={ref} className='scroll-mt-28 sm:mb-40 mb-28' id='experience'>
@@ -25,24 +41,50 @@ export default function Experience() {
                 contentStyle={{
                   background: theme === 'light' ? "#f3f4f6" : 'rgba(255,255,255,0.05)',
                   boxShadow: "none",
+                  borderRadius: '0.75rem',
                   border: `1px solid ${theme === 'light' ? 'rgba(0, 0, 0, 0.05)' : 'rgba(255,255,255,0.15)'}`,
                   textAlign: "left",
                   padding: "1.3rem 2rem"
                 }}
                 contentArrowStyle={{
-                  borderRight: `0.4rem solid ${theme === 'light' ? '#9ca3af' : 'rgba(255,255,255,0.05)'}`
+                  borderRight: `0.4rem solid ${theme === 'light' ? '#9ca3af' : 'rgba(255,255,255,0.25)'}`
                 }}
                 date={item.date}
                 icon={item.icon}
                 iconStyle={{
-                  background: theme === 'light' ? "white" : 'rgba(255,255,255,0.15)',
+                  background: theme === 'light' ? "white" : 'black',
                   fontSize: "1.5rem",
-                  color: 'rgba(255,255,255,0.75)'
+                  color: `${item.iconColor}`
                 }}
               >
-                <h3 className='font-semibold capitalize'>{item.title}</h3>
-                <p className='font-normal !mt-0'>{item.location}</p>
-                <p className='!mt-1 !font-normal text-gray-700 dark:text-white/60'>{item.description}</p>
+                <h3 className='font-semibold capitalize text-2xl'>{item.title}</h3>
+                <div className='flex justify-between items-center'>
+                  <p className='font-normal !mt-0 text-xl'>{item.company}</p>
+                  <p className='!text-xs !mt-0'>{item.location}</p>
+                </div>
+                <p
+                  className='!mt-1 !font-normal text-gray-700 dark:text-white/60'
+                >
+                  {showMore.includes(i) ? item.description : shortDescription(item.description)}{' '}
+
+                  <span
+                    className='text-sm underline underline-offset-2 cursor-pointer'
+                    onClick={handleShowMore(i)}
+                  >
+                    {showMore.includes(i) ? 'show less' : 'show more'}
+                  </span>
+                </p>
+                <ul className='flex flex-wrap gap-2 !mt-2 sm:mt-auto'>
+                  {item.skills.slice(0, 9).map((skill, i) => (
+                    <li
+                      key={i}
+                      className='dark:bg-white/[0.1] bg-black text-white px-3 py-1 text-[0.65rem] uppercase 
+                                tracking-wider rounded-lg dark:text-white/70'
+                    >
+                      {skill}
+                    </li>
+                  ))}
+                </ul>
               </VerticalTimelineElement>
             </React.Fragment>
           ))
--- lib/data.ts
+++ lib/data.ts
@@ -1,6 +1,6 @@
 import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
-import { FaReact } from "react-icons/fa";
+import { FaAngular, FaReact, FaVuejs } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
 import corpcommentImg from "@/public/corpcomment.png";
 import rmtdevImg from "@/public/rmtdev.png";
@@ -35,28 +35,69 @@ export const links = [
 
 export const experiencesData = [
     {
-        title: "Graduated bootcamp",
-        location: "Miami, FL",
+        title: "Frontend Developer",
+        company: "Capillary Technologies",
+        location: "Hyderabad, India",
         description:
-            "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
-        icon: React.createElement(LuGraduationCap),
+            "Built a framework to develop PWA's in a shorter time by creating reusable modules like Auth, Search, Filter, Cart, Checkout, Payments, etc,. Worked on multiple projects like PizzaHut, FabIndia, KFC and SellerGyan.",
+        icon: React.createElement(FaAngular),
+        iconColor: "rgb(223, 21, 56)",
+        date: "2017 - 2018",
+        skills: ['Angular', 'TypeScript', 'RxJS', 'Angular Material', 'Ionic', 'Webpack', 'HTML', 'Bootstrap', 'Node', 'Express']
+    },
+    {
+        title: "Senior Front-end Developer",
+        company: "CatchThatBus",
+        location: "Hyderabad, India",
+        description:
+            "Worked on CatchThatBus web app, added few new features to the app. And worked on legacy website and integrated internal CMS. Worked on multiple projects of the company and mostly on PWA 'Trokka', it's a Paytm of Malaysia to book Bus, Train, Tours & Mobile recharge, etc..",
+        icon: React.createElement(FaVuejs),
+        iconColor: "rgb(72, 187, 135)",
         date: "2019",
+        skills: ['JavaScript', 'Vue.js', 'Nuxt.js', 'Vuex', 'Quasar', 'AWS', 'Docker', 'Git', 'Shell script']
     },
     {
-        title: "Front-End Developer",
-        location: "Orlando, FL",
+        title: "Software Engineer",
+        company: "Creative Mindz Group",
+        location: "Hyderabad, India",
         description:
-            "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
-        icon: React.createElement(CgWorkAlt),
-        date: "2019 - 2021",
-    },
-    {
-        title: "Full-Stack Developer",
-        location: "Houston, TX",
-        description:
-            "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
+            "Worked on multiple small products to help e-commerce sellers to ease there work. Mainly worked on a platform named SellerGyan to improve the sales for e-commerce sellers. Did data scraping and developed the web applications frontend and even worked on the backend using NodeJS and Express.",
         icon: React.createElement(FaReact),
-        date: "2021 - present",
+        iconColor: "rgb(103, 219, 251)",
+        date: "2019 - 2020",
+        skills: ['JavaScript', 'React.js', 'Redux', 'Material-UI (MUI)', 'Next.js', 'Angular', 'TypeScript', 'Webpack', 'NodeJS', 'Express', 'MongoDB']
+    },
+    {
+        title: "Freelance Developer",
+        company: "Tez Solutions, Minto and others",
+        location: "Remote",
+        description:
+            "Converted a web applications to a hybrid mobile apps using ionic. Built new SPA's using company's inclusive pre built web app/hybrid app building framework. Used React Native to built an app. For Minto.ai I developed an Admin tool/dashboard to track & monitor the condition of industrial rotating machines. For the E-commerce client I built a tool for optimizing the product image and printing the design.",
+        icon: React.createElement(FaAngular),
+        iconColor: "rgb(223, 21, 56)",
+        date: "2020",
+        skills: ['Angular', 'TypeScript', 'Ionic', 'Angular Material', 'RxJS', 'React.js', 'Redux.js', 'React Native', 'JavaScript', 'Bootstrap']
+    },
+    {
+        title: "Frontend Engineer",
+        company: "iSpace, Inc.",
+        location: "Remote",
+        description: "Worked on Inseego Connect, it's a cloud-based configuration and monitoring application provided by Inseego. Maintained web app by fixing issues encountered by QA folks and implementing new features like creating charts and filters. Wrote test cases to improve the code coverage.",
+        icon: React.createElement(FaReact),
+        iconColor: "rgb(103, 219, 251)",
+        date: "2021",
+        skills: ['JavaScript', 'React.js', 'Next.js', 'chakra UI', 'Jest', 'Cypress', 'Vue.js', 'SASS', 'HTML', 'Webpack']
+    },
+    {
+        title: "Senior Front-end Developer",
+        company: "Blokbot",
+        location: "Remote",
+        description:
+            "Build the main web front application for blokbot. This web application allows users to interact with different auto trading bots and track the performance of each bot.",
+        icon: React.createElement(FaReact),
+        iconColor: "rgb(103, 219, 251)",
+        date: "2021 - 2022",
+        skills: ['TypeScript', 'React.js', 'Next.js', 'React Query', 'Tailwind CSS', 'HTML', 'Webpack']
     },
 ] as const;
 
