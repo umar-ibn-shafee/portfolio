352dde86f2e03b334ed90fd680764ae3c823f113
md.umar05@gmail.com <md.umar05@gmail.com>
2023-08-29 14:06:51 +0530

    footer responsiveness

--- components/screens/footer.tsx
+++ components/screens/footer.tsx
@@ -1,16 +1,20 @@
+'use client'
+
 import { socials } from '@/lib/data'
 import React from 'react'
+import FlowText from '../generic/flow-text'
 
 export default function Footer() {
   return (
-    <footer className='mb-6 px-12 text-gray-500 dark:text-white flex justify-between items-center'>
-      <div className='flex flex-col items-start gap-2 text-xs pr-4'>
+    <footer className='mb-6 px-12 text-gray-500 dark:text-white flex flex-col-reverse gap-8 sm:flex-row sm:justify-between items-start'>
+      <div className='flex flex-col items-start gap-2 text-xs'>
         <div className='uppercase font-thin'>Version</div>
         <small className='block text-xs'>
           2023 &copy; Mohammed Umar.
           {/* <br /> All rights reserved */}
         </small>
       </div>
+      <div className='h-[0.05rem] w-[100%] bg-black dark:bg-slate-50 rounded-xl sm:hidden'></div>
       <div className='flex flex-col items-start gap-2 text-xs pr-4'>
         <div className='uppercase font-thin'>Socials</div>
         <div className='w-full flex flex-row gap-4 font-medium'>
@@ -20,7 +24,8 @@ export default function Footer() {
               href={social.link}
               target='_blank'
             >
-              {social.name}
+              {/* {social.name} */}
+              <FlowText text={social.name} onHover={true} />
             </a>
           ))}
         </div>
